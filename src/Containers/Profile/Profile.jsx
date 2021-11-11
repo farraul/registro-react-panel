1fd0d2128414ef528eeb0357b7679aa45723d195
raul farl <raulfarllobell@gmail.com>
2021-11-11 07:55:45 +0100

    configure redux, get data from profile

--- src/Containers/Profile/Profile.jsx
+++ src/Containers/Profile/Profile.jsx
@@ -2,24 +2,25 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { connect } from 'react-redux';
+import { LOGOUT } from '../../redux/types';
 import './profile.scss';
-
+import profile from '../../images/profile.png';
 
 
 
 const Profile = (props) => {
     
-    {/*imprimir datos personales*/}
-    //Hook 
-    //const [datosPerfil, setDatosPerfil] = useState(JSON.parse(localStorage.getItem("datosLogin")));
-    //props.credentials?.usuario?.nombre
-    //<p>{props.credentials.user?.name}</p>
-
-
     useEffect(() => {
-        console.log("imprimimos props: ",props);
     }, [])
 
+
+//logout
+    const logOut = () => {
+        //vaciamos redux. Así ya no estamos logueados
+        props.dispatch({type:LOGOUT});
+    }
+    
+    
 
     {/*modificar datos*/}
     //Hooks
@@ -35,10 +36,11 @@ const Profile = (props) => {
     }
 
     {/*función actualizar usuario*/}
-    const update = async () => {
+    const update = async () => 
+    {
         //Generación del body
         let body = {
-            id: user.id,
+            id: props.data_user?.user?._id,
             name: user.name,
             email: user.email,
             telf: user.telf,
@@ -47,10 +49,9 @@ const Profile = (props) => {
         console.log("ENVIANDO AL BACKEND ESTO....",body);
         
         try {
-            let res = await axios.put(`https://app-movies-mongoose.herokuapp.com/usuario/${props.credentials}`, body);
+            let res = await axios.put(`https://app-movies-mongoose.herokuapp.com/usuario/${props.data_user?.user?._id}`, body);
             //Guardado de datos en localStorage
             console.log("dentro del try", res);
-            
         } catch (error) {
 
             console.log("error de front", error);
@@ -65,7 +66,7 @@ const Profile = (props) => {
             console.log("ENVIANDO AL BACKEND ESTO....",body);
         
             try {
-                let res = await axios.delete(`https://app-movies-mongoose.herokuapp.com/usuario/${props.credentials}`, body);
+                let res = await axios.delete(`https://app-movies-mongoose.herokuapp.com/usuario/${props.data_user?.user?._id}`, body);
                 //Guardado de datos en localStorage
                 console.log("dentro del try", res);
                /* localStorage.removeItem("datosLogin");
@@ -99,37 +100,11 @@ const Profile = (props) => {
  console.log("entre aqui.");
     
     return (
-             <div className="designProfile"> {/* no hay datos*/} 
-                    {/* {!datosPerfil
-                            ? <div id="table-profile">
-                            <h1>Datos personales</h1>
-                            <div className="table-row">
-                                <div className="table-field-1">Nombre:</div>
-                                <div>{' '}</div> //*igual que el de abajo, el "?" es el if, el ":" es un else, si hay algo dentro imprime sino lo otro (condicional ternario)  
-                            </div>
-                            <br />
-                            <div className="table-row">
-                                <div className="table-field-1">Email:</div>
-                                <div>{' '}</div>
-                            </div>
-                            <br />
-                            <div className="table-row">
-                                <div className="table-field-1">Teléfono:</div>
-                                <div>{' '}</div>
-                            </div>
-                            <br />
-                            <div className="table-row">
-                                <div className="table-field-1">Id:</div>
-                                <div>{' '}</div>
-                            </div>
-                        </div>
-                    
-                        :  
-                    */}
-
-
-
+            <div className="main-container">        
+             <div className="designProfile">  
                     <div id="table-profile">
+                    <img className="img-profile" src={profile} alt="profile"/>
+
                         <h1>Datos personales</h1>
                         <div className="table-row">
                             <div className="table-field-1">Nombre:</div>
@@ -152,18 +127,70 @@ const Profile = (props) => {
                             <div className="table-field-1">Id:</div>
                             <div>{props.data_user?.user?._id}</div>
                         </div>
-
-                        
+                        <div className="user-logout" onClick={()=>logOut()}>LOGOUT</div>
                     </div>
                  </div>
+
+                 <div className="container-update-profile">
+
+{/*<pre>{JSON.stringify(user, null, 2)}</pre>*/}
+<div className="table-update">
+    <h1>Actualizar</h1>
+    <input className="input-form-update" type='text' name='name' title='name' onChange={userHandler} lenght='30' placeholder='Nombre' />
+    <br />
+    <input className="input-form-update" type='email' name='email' title='email' onChange={userHandler} lenght='30' placeholder='Email' />
+    <br />
+    <input className="input-form-update" type='text' name='telf' title='telf' onChange={userHandler} lenght='30' placeholder='Teléfono' />
+    <br/>
+    <div className="sendButton" onClick={() => update()}>Actualizar datos</div>
+    <div className="deleteButton" onClick={() => deleteuser()}>Borrar usuario</div>
+   {/* <div className="deleteButton" onClick={() => deletealluser()}>Borrar todos usuario db</div> */}
+</div>
+</div>
+                
+
+
+
+
+
+
+
+
+                 </div>
+
+
             )
     }
                 else
                 {
                     return (
-                        <div className="designProfile">
-                            NADIE SABE NADA DE NINGÚN USUARIO---
+                        <div className="designProfile">  
+                        <div id="table-profile">
+                            <h1>Datos personales</h1>
+                            <div className="table-row">
+                                <div className="table-field-1">Nombre:</div>
+                    
+                                <div></div>
+                            </div>
+                            
+                            <br />
+                            <div className="table-row">
+                                <div className="table-field-1">Email:</div>
+                                <div></div>
+                            </div>
+                            <br />
+                            <div className="table-row">
+                                <div className="table-field-1">Teléfono:</div>
+                                <div></div>
+                            </div>
+                            <br />
+                            <div className="table-row">
+                                <div className="table-field-1">Id:</div>
+                                <div></div>
+                            </div>
+                            <div className="user-logout" onClick={()=>logOut()}>LOGOUT</div>
                         </div>
+                     </div>
                     )
                 }
 
@@ -174,7 +201,7 @@ const Profile = (props) => {
 
             {/*Parte de actualizar */}
 
-           
+            return (
             <div className="container-update-profile">
 
                 {/*<pre>{JSON.stringify(user, null, 2)}</pre>*/}
@@ -192,7 +219,7 @@ const Profile = (props) => {
                 </div>
             </div>
             
-       
+            )
 
 
     
