7d9fedc8fb31c5cd494c7fcd5110c8229b12f387
raul farl <raulfarllobell@gmail.com>
2021-11-14 23:58:02 +0100

    create the search home

--- src/Containers/Home/Home.jsx
+++ src/Containers/Home/Home.jsx
@@ -6,27 +6,79 @@ import { useNavigate } from 'react-router-dom';
 const Home = () => {
 
     const history = useNavigate();
-    /*
-            let body = {
-                email: credentials.correo,
-                password: credentials.clave
-            };*/
+
     const [msgError, setmsgError] = useState("");
     const [datosperfil, setDatosPerfil] = useState("");
+    const [credentials, setCredentials] = useState({peli:''});
+    const [films, setfilms]= useState("");
+    const [filmfound, setfilmfound]  = useState("");
 
-    const [user, setUser] = useState({
-        id: '',
-        name: '',
-        email: '',
-        telf: ''
-    });
 
     useEffect(() => {
-        traePeliculas();
+        bringfilms (); 
+        datauser();
+
+
     }, []);
 
 
-    const traePeliculas = async () => {
+    let arr = ['pepe','maria','ramon','gutavo']
+ 
+
+ 
+    const bringfilms = async () => {
+        let res = await axios.get("https://api.themoviedb.org/3/movie/popular?api_key=51c1099989a6923f3d12154210fc2cf7&language=en-US&page=1");
+        setfilms(res.data.results);
+        console.log("datos de pelis",res.data.results);      
+        
+};
+
+
+
+
+    const writefilm = (e) =>{
+  
+        setCredentials( e.target.value );
+
+        let film= credentials;
+        console.log("cred", credentials)
+      
+
+         const filtered = films.filter( word => {
+            //return word.toLowerCase().match(films.toLowerCase())
+            console.log("word", word.original_title);
+            console.log("film: ", film);
+            console.log("word.original_title:", word.original_title)
+         
+           return word.original_title.match(film);
+
+           
+          })
+          console.log("filtered2:",filtered)
+          const setfilmfound=filtered;
+          //const container = document.getElementById('solutions')
+          //container.innerHTML= filtered
+
+
+        /*filtered.map((peli) => {
+
+             //container.innerHTML= "o";
+             console.log("ho:",peli.original_title);
+             
+            let container = document.getElementById('solutions');
+          container.innerHTML= peli.original_title;
+             
+              
+         
+
+        })*/
+
+    }
+
+    
+
+
+    const datauser = async () => {
         try {
             let res = await axios.get("https://app-movies-mongoose.herokuapp.com/usuario/");
             setDatosPerfil(res.data);
@@ -36,53 +88,106 @@ const Home = () => {
         }
     };
 
-if (datosperfil.length>0) {
     return (
+        
         <div className='container-home pt-2'>
             <div>
                 <h1 className='h1-home mt-2'>Tu plataforma de peliculas</h1>
+                <div className="container-buscador">
+                    
+                    <input className="imput-search" type="text" name="film" onChange={writefilm} title="film" lenght="30" placeholder="Escribe pelicula"/>
+                    <div id="solutions" className="container-equals-films">
+                    
+       {/*  {[filmfound].map((peli) => {
+           return (
+            <div className="peli">
+            </div>
+            )
+         }) */}
+ 
+         } 
+        
+         
+          
+ 
 
+
+
+
+
+
+                    </div>
+                </div>
 
                 <p className="text-center mt-10">últimos usuarios registrados </p>
-
                 
-                <div id="table-home-print">
-                    <div class="colum-home-print">
-                        
-                        {datosperfil.map(run => {
-                            return (
-                                <p className="colum-components-home-print" key={run._id}>
-                                    Nombre: {run.name}
-                                </p>
-                            )
-                        })}
+
+                { datosperfil.length > 0 &&
+                    <div id="table-home-print">
+                        <div className="colum-home-print">
+                            
+                            {datosperfil.map(run => {
+                                return (
+                                    <p className="colum-components-home-print" key={run._id}>
+                                        Nombre: {run.name}
+                                    </p>
+                                )
+                            })}
+                        </div>
+                        <div className="colum-home-print">
+                            {datosperfil.map(run => {
+                                return (
+                                    <p className="colum-components-home-print" key={run._id}>
+                                        Email: {run.email}
+                                    </p>
+                                )
+                            })}
+                        </div>
+                        <div className="colum-home-print">
+                            {datosperfil.map(run => {
+                                return (
+                                    <p className="colum-components-home-print" key={run._id}>
+                                        Id: {run._id}
+                                    </p>
+                                )
+                            })}
+                        </div> 
                     </div>
-                    <div class="colum-home-print">
-                        {datosperfil.map(run => {
-                            return (
-                                <p className="colum-components-home-print" key={run._id}>
-                                    Email: {run.email}
-                                </p>
-                            )
-                        })}
-                    </div>
-                    <div class="colum-home-print">
-                        {datosperfil.map(run => {
-                            return (
-                                <p className="colum-components-home-print" key={run._id}>
-                                    Id: {run._id}
-                                </p>
-                            )
-                        })}
-                    </div> 
-                </div>
+                }
+            
             </div>
         </div>
     )
-}
+
 return(
-    <div></div>
+    <div style={{color: 'red'}}>asfdsf</div>
 )
+
+
+{/*}
+let arr = ['adios', 'hola', 'uno', 'dos', 'tres']
+
+const search = 'no'
+search =()=>{
+
+const filtered = arr.filter( word => {
+  return word.toLowerCase().match(search.toLowerCase())
+})
+
+const container = document.getElementById('solutions')
+container.innerHTML = filtered
+
+console.log(container)
+}
+
+
+
+    <div>holaa</div>
+    <div className="" onClick={() => search()}>Login</div>
+
+*/}
+
+
 
 };
 
