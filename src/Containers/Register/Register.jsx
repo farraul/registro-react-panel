16c81658caa52ab12972527ec08b95398688e241
raul farl <raulfarllobell@gmail.com>
2021-11-22 20:52:54 +0100

    changes in register, add condition od name, the name must to have 4 digits, and the email the have to be real

--- src/Containers/Register/Register.jsx
+++ src/Containers/Register/Register.jsx
@@ -9,43 +9,80 @@ const Register = () => {
 
     //Hooks
     const [msgError, setmsgError] = useState("");
+
+    const [inputs_data_form, setinputs_data_form] = useState({
+        name: '',
+        email: '',
+        telf:'',
+        password: '',
+    });
+
     const [user, setUser] = useState({
         name: '',
         email: '',
+        telf:'',
         password: '',
     });
 
     //Manejadores o Handlers
-
     const userHandler = (e) => {
         setUser({ ...user, [e.target.name]: e.target.value });
     }
 
+    const change_name = ()=>{
+        console.log("user: ",user)
+        console.log("user.name: ",user.name)
+        console.log("length de user: ",user.name.length)
+
+
+        if((user.name.length>=3) && (/^[a-z]/gi.test(user.name))){ //no funciona el filtro de letras
+           setinputs_data_form({
+            ...inputs_data_form,
+            name:"✓ Nombre"});
+
+        }else{
+            setinputs_data_form({
+            ...inputs_data_form,
+            name:"✗ Utiliza solo letras y minimo 4 caracteres"});
+     
+        };
+    }
+
+    const change_email = ()=>{
+        if( /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/.test(user.email)) {
+            setinputs_data_form({
+                ...inputs_data_form,
+                email:"✓ Email"});
+         }
+         else{
+     
+
+        setinputs_data_form({
+            ...inputs_data_form,
+            email:"✗ Email incorrecto"});
+         console.log("else 2:",inputs_data_form.email);
+        };
+    }
+    
+
+
+
+
     //useEffect
 
     useEffect(() => {
-        //UseEffect que se ejecuta sólo una vez al montarse. (1a vez).
+        console.log("inputs_data_form:",inputs_data_form)
 
     }, []);
 
     useEffect(() => {
-        //UseEffect que se ejecutará CADA VEZ que se actualize el estado.(renderizando de nuevo).
+        console.log("user: ",user)
+        console.log("inputs_data_form:",inputs_data_form)
     });
 
     //Funciones
 
     const enviaDatosRegistro = async () => {
-        //Comprobación de errores en los datos
-
-        if (! /[a-z]/gi.test(user.name) ) {
-           setmsgError("Nombre Incorrecto");
-           return;
-        };
-        
-        if (! /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/.test(user.email) ) {
-           setmsgError("Email Incorrecto");
-           return;
-        };
 
         //Generación del body
         let body = {
@@ -57,8 +94,6 @@ const Register = () => {
 
         //Conexion a axios y envio de datos
         console.log("ENVIANDO AL BACKEND ESTO....",body);
-        
-        
         try {
             let res = await axios.post("https://app-movies-mongoose.herokuapp.com/api/signup", body);
             console.log("imprimir res: ",res)
@@ -66,17 +101,10 @@ const Register = () => {
             localStorage.setItem("datosLogin", JSON.stringify(res.data.user));
             setmsgError("Usuario registrado con éxito");
      
-            history("/login");
-    
-            
+            history("/login");  
         } catch (error) {
             console.log(error)
         }
-        
-
-        
-
-       
     };
 
 
@@ -86,14 +114,24 @@ const Register = () => {
             <div id="container-form">
                 {/*<pre>{JSON.stringify(user, null, 2)}</pre>*/}
                 <h1>Registrate</h1>
-                <input className="input-form-register" type='text' name='name' title='name' onChange={userHandler} lenght='30' placeholder='Nombre' />
-                <input className="input-form-register" type='email' name='email' title='email' onChange={userHandler} lenght='30' placeholder='Email' />
-                <input className="input-form-register" type='text' name='telf' title='telf' onChange={userHandler} lenght='30' placeholder='Teléfono' />
-                <input className="input-form-register" type='text' name='password' title='password' onChange={userHandler} lenght='30' placeholder='Password' />
-            
+                <div className="container-form-2fields">
+                    <div className="input-form-register-fields">
+                        <input className="input-form-register" type='text' name='name' title='name' onChange={e =>{userHandler(e); change_name()}} lenght='30' placeholder='Nombre' />
+                        <div className="div-print-info-correct">{inputs_data_form.name}</div>
+                        <input className="input-form-register" type='email' name='email' title='email' onChange={e =>{userHandler(e); change_email()}} lenght='30' placeholder='Email' />
+                        <div className="div-print-info-correct">{inputs_data_form.email}</div>
+                        <input className="input-form-register" type='text' name='telf' title='telf' onChange={userHandler} lenght='30' placeholder='Teléfono' />
+                        <div className="div-print-info-correct">{inputs_data_form.telf}</div>
+                        <input className="input-form-register" type='text' name='password' title='password' onChange={userHandler} lenght='30' placeholder='Password' />
+                        <div className="div-print-info-correct">{inputs_data_form.password}</div> 
+                    </div>
+                    
+                </div>
                 <div className="sendButton" onClick={() => enviaDatosRegistro()}>Registrame</div>
+
+
             </div>
-            <div>{msgError}</div>
+
         </div>
     )
 };
